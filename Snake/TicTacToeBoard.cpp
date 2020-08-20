c62981ac3671754b920c5d618d2e0cc9cc45356b
Jonathan Bender <jonathon.bender@rockets.utoledo.edu>
2020-08-19 20:15:37 -0400

    Implemented very basic board state logic.

--- Snake/TicTacToeBoard.cpp
+++ Snake/TicTacToeBoard.cpp
@@ -1,4 +1,36 @@
 #pragma once
 #include "pch.h"
 
-#include "GUI_Test.h"
+#include "TicTacToeBoard.h"
+#include <array>
+
+bool TicTacToeBoard::isEmpty(int xCoordinate, int yCoordinate) {
+	int playerIndex = getPlayerIndexOf(xCoordinate, yCoordinate);
+
+	return playerIndex == 1 || playerIndex == 2;
+}
+
+int TicTacToeBoard::getPlayerIndexOf(int xCoordinate, int yCoordinate) {
+	int boardStateIndex = 3 * yCoordinate + xCoordinate;
+	int playerIndex = boardState[boardStateIndex];
+
+	return playerIndex;
+}
+
+char TicTacToeBoard::getPlayerSymbolOf(int xCoordinate, int yCoordinate) {
+	int playerIndex = getPlayerIndexOf(xCoordinate, yCoordinate);
+
+	return PLAYER_SYMBOLS[playerIndex];
+}
+
+std::array<std::array<char, 3>, 3> TicTacToeBoard::getSymbolicBoardState() {
+	std::array<std::array<char, 3>, 3> symbolicBoardState;
+
+	for (int y = 0; y < 3; y++) {
+		for (int x = 0; x < 3; x++) {
+			symbolicBoardState[y][x] = getPlayerSymbolOf(x, y);
+		}
+	}
+
+	return symbolicBoardState;
+}
---
+++ Snake/TicTacToeBoard.h
@@ -0,0 +1,26 @@
+#pragma once
+#include "pch.h"
+
+#include "MatrixPrinter.h"
+#include <array>
+
+
+
+class TicTacToeBoard {
+	std::array<char, 3> PLAYER_SYMBOLS = { ' ', 'X', 'O' };
+
+	int boardState[9] = { 0 };
+	Screen::MatrixPrinter boardPrinter;
+	Coordinate initialPosition = { 2,2 };
+
+public:
+	TicTacToeBoard();
+
+private:
+	bool isEmpty(int xCoordinate, int yCoordinate);
+	bool isGameOver(int xCoordinate, int yCoordinate);
+	int whichPlayerWins(int xCoordinate, int yCoordinate);
+	int getPlayerIndexOf(int xCoordinate, int yCoordinate);
+	char getPlayerSymbolOf(int xCoordinate, int yCoordinate);
+	std::array<std::array<char, 3>, 3> getSymbolicBoardState();
+};
